19e08ae5d5ef7262926408216d2681da3669652b
olamilekan-adeleke <olamilekanadeleke123@gmail.com>
2021-11-26 13:08:12 +0100

    update

--- app.js
+++ app.js
@@ -1,28 +1,22 @@
-const express = require('express');
-const morgan = require('morgan');
+const express = require("express");
+const morgan = require("morgan");
 
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
-
 
 // middle-wares
 const app = express();
 app.use(express.json());
-app.use(morgan('dev'));
-
+app.use(morgan("dev"));
 
 // routes...
 
-app.get('/', (req, res, next) => { 
-    res.send('server is running');
+app.get("/", (req, res) => {
+  res.send("server is running");
 });
-
-
-
 
 // start server
-const port  = process.env.PORT || 8080;
-app.listen(port, () => { 
-    console.log(`Express server listening on port ${port}`);
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`Express server listening on port ${port}`);
 });
-
